5178a7f238868b42d393e9848532a1b6df7c58ac
dw61 <wltok@live.com>
2022-09-25 14:43:50 -0400

    all cities

--- openings.py
+++ openings.py
@@ -5,11 +5,12 @@ from selenium import webdriver
 
 url = "https://www.amazon.jobs/en/search?&category%5B%5D=software-development&job_type%5B%5D=Full-Time&country%5B%5D=USA"
 moreTeamsButton = "#main-content > div.search-page > div > div > div.container > content > div > div > div.d-none.d-md-block.col-sm-4.search-page-filter > div.search-filters > div:nth-child(9) > div > div.show-all > fieldset > button"
-cities = ["Seattle", "New York", "Arlington", "Austin", "Bellevue", "Sunnyvale", "Boston", "East Palo Alto", "San Francisco", "Dallas", "Cupertino", "Herndon", "Santa Clara", "Redmond", "San Diego", "Santa Monica", "Culver City", "Atlanta", "Irvine", "Jersey City", "Denver", "Palo Alto", "Portland", "Tempe", "Newark", "Minneapolis", "Nashville", "Boulder", "Detroit", "Chicago", "Miami", "Cambridge", "San Luis Obispo"]
+cities = ["Seattle", "New York", "Arlington", "Austin", "Bellevue", "Sunnyvale", "Boston", "East Palo Alto", "San Francisco", "Dallas", "Cupertino", "Herndon", "Santa Clara", "Redmond", "San Diego", "Santa Monica", "Culver City", "Atlanta", "Irvine", "Jersey City", "Denver", "Palo Alto", "Portland", "Tempe", "Newark", "Minneapolis", "Nashville", "Boulder", "Detroit", "Chicago", "Miami", "Cambridge", "San Luis Obispo", "North Reading", "Hawthorne", "Westborough", "Los Angeles", "Pennsylvania Furnace", "Northridge", "Annapolis Junction", "Houston", "Phoenix", "Raleigh", "Troy", "Santa Cruz", "Charlotte", "Florence", "Hudson", "Pasadena", "Pittsburgh", "San Jose", "Lehi", "Salt Lake City", "West Valley City", "Chantilly", "Charleston", "Durham", "Louisville", "North Charleston", "Philadelphia", "Washington", "Auburn", "Berkeley", "Brisbane", "Cincinnati", "Glendale", "Grand Haven", "Hebron", "Hilliard", "Las Vegas", "Madison", "Malvern", "Miami Beach", "Orlando", "Santa Barbara", "Tukwila", "Vancouver", "West Hollywood", "Wilmington"]
 openings = {}
 
 for city in cities:
 
+    print(city)
     driver = webdriver.Safari()
     driver.get(f"{url}&city%5B%5D={city.replace(' ', '%20')}")
 
