eb1ee045b010fe43097895132970d729902bf423
dw61 <wltok@live.com>
2022-09-25 14:07:15 -0400

    update openings.json

--- openings.py
+++ openings.py
@@ -3,19 +3,15 @@ import bs4
 import selenium
 from selenium import webdriver
 
-url = "https://www.amazon.jobs/en/search?&category%5B%5D=software-development&job_type%5B%5D=Full-Time&country%5B%5D=USA&city%5B%5D={CITY}"
+url = "https://www.amazon.jobs/en/search?&category%5B%5D=software-development&job_type%5B%5D=Full-Time&country%5B%5D=USA"
 moreTeamsButton = "#main-content > div.search-page > div > div > div.container > content > div > div > div.d-none.d-md-block.col-sm-4.search-page-filter > div.search-filters > div:nth-child(9) > div > div.show-all > fieldset > button"
-cities = [
-    "Seattle",
-    "New York",
-    "Arlington"
-]
+cities = ["Seattle", "New York", "Arlington", "Austin", "Bellevue"]
 openings = {}
 
 for city in cities:
 
     driver = webdriver.Safari()
-    driver.get(url.replace("{CITY}", city.replace(" ", "%20")))
+    driver.get(f"{url}&city%5B%5D={city.replace(' ', '%20')}")
     button = driver.find_element("css selector", moreTeamsButton)
 
     while True:
